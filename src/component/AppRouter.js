9d4d3f4d15fa4b4fc9b3df49ae81d22c5e1213c9
Ziad-Mid <elmidaoui.ziad@gmail.com>
2021-10-25 23:17:56 +0200

    .

--- src/component/AppRouter.js
+++ src/component/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter as Router , Switch } from 'react-router-dom'
+import { Route, BrowserRouter as Router , Switch,Redirect } from 'react-router-dom'
 import About from './About'
 import Contact from './Contact'
 import Home from './Home'
@@ -18,6 +18,7 @@ function AppRouter() {
                 <Route exact path='/projects' component={Projects} />
                 <Route exact path='/contact' component={Contact} />
                 <Route component={NotFound} />
+                <Redirect to="/" />
             </Switch>
         
         </>
